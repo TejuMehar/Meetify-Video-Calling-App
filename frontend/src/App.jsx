b83e89f9dab083204e0844396e4e70200a8b4465
Tejas mehar <tejas mehar7@gmail.com>
2025-09-09 10:23:34 +0530

    Authentication sign in page template is imported from Material ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/landingPage'
+import Authentication from './pages/authentication' 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/auth' element={<Authentication />} />
       </Routes>
     </Router>
   )
